05a26acbd6e553a8689fc5e4deb3e865de0c4afa
jeff <jgoettsch@gmail.com>
2012-09-18 20:40:15 -0700

    Prowl client now raises a ProviderKeyError when retrieve_token or retreive_apikey is called with a bad Provider Key.

--- pushnotify/tests/tests.py
+++ pushnotify/tests/tests.py
@@ -198,7 +198,7 @@ class ProwlTest(unittest.TestCase):
         # invalid providerkey
 
         self.client.providerkey = self.client.providerkey[0:-1]
-        self.assertRaises(exceptions.ApiKeyError,
+        self.assertRaises(exceptions.ProviderKeyError,
                           self.client.retrieve_apikey, PROWL_REG_TOKEN)
 
     def test_retrieve_token_valid(self):
@@ -218,7 +218,7 @@ class ProwlTest(unittest.TestCase):
         """
 
         self.client.providerkey = self.client.providerkey[0:-1]
-        self.assertRaises(exceptions.ApiKeyError,
+        self.assertRaises(exceptions.ProviderKeyError,
                           self.client.retrieve_token)
 
     def test_verify_user_valid(self):
