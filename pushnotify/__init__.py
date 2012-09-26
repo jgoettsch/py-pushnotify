1273ccf3a33f30e22644e132e09185c405ba70ce
jeff <jgoettsch@gmail.com>
2012-09-25 19:35:00 -0700

    Added get_client factory method to pushnotify.

--- pushnotify/__init__.py
+++ pushnotify/__init__.py
@@ -5,5 +5,38 @@ license: BSD, see LICENSE for details.
 
 import logging
 
+import nma
+import prowl
+import pushover
+
 
 logger = logging.getLogger(__package__)
+
+
+def get_client(type_, application):
+    """Get a pushnotify client of the specified type.
+
+    Args:
+        type_: A string containing the type of client to get. Valid
+            types are 'nma,' 'prowl,', and 'pushover,' for Notify My
+            Android, Prowl, and Pushover clients, respectively.
+        application: A string containing the name of the application on
+            behalf of whom the client will be sending messages.
+
+    Returns:
+        An nma.Client, prowl.Client, or pushover.Client.
+
+    """
+
+    type_ = type_.lower()
+
+    if type_ == 'nma':
+        return nma.Client(application)
+    elif type_ == 'prowl':
+        return prowl.Client(application)
+    elif type_ == 'pushover':
+        return pushover.Client(application)
+
+
+if __name__ == '__main__':
+    pass
