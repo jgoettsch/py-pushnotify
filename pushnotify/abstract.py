512574a51c01a0b0699087d3fdbc4833ba200b63
Jeffrey Goettsch <jgoettsch@gmail.com>
2013-01-31 22:59:09 -0800

    Switched license to GNU GPL v3.

--- pushnotify/abstract.py
+++ pushnotify/abstract.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
-# vim: set fileencoding=utf-8
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Jeffrey Goettsch and other contributors.
+#
+# py-pushnotify is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# py-pushnotify is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with py-pushnotify.  If not, see <http://www.gnu.org/licenses/>.
 
 """Module for abstract class.
-
-copyright: Copyright (c) Jeffrey Goettsch and other contributors.
-license: BSD, see LICENSE for details.
 
 """
 
